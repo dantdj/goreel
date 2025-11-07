fb4708ce8af2342c5f41cf2d74de13e300bfeba4
Daniel Temple <dantdj@gmail.com>
2025-11-07 12:42:18 +0000

    Start logging to axiom

--- main.go
+++ main.go
@@ -4,20 +4,33 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/axiomhq/axiom-go/axiom"
 	"github.com/dantdj/goreel/api"
 	"github.com/joho/godotenv"
+
+	adapter "github.com/axiomhq/axiom-go/adapters/slog"
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
-
 	err := godotenv.Load()
 	if err != nil {
 		slog.Error("Error loading .env file", slog.String("error", err.Error()))
 		return
 	}
 
+	client, err := axiom.NewClient(
+		axiom.SetPersonalTokenConfig(os.Getenv("AXIOM_TOKEN"), os.Getenv("AXIOM_ORG_ID")),
+	)
+
+	handler, err := adapter.New(
+		adapter.SetDataset(os.Getenv("AXIOM_DATASET")),
+		adapter.SetClient(client),
+	)
+	defer handler.Close()
+
+	//logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
 	// Start the HTTP server.
 	if err := api.Serve(8089); err != nil {
 		slog.Error("Failed to start server", slog.String("error", err.Error()))
